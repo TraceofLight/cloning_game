ee5487629b48a0f836ab5005e63d2dd36cee2bfe
TraceofLight <rickyjun96@gmail.com>
2024-07-14 00:11:06 +0900

    feat: init commit
    기초적인 세팅  추가

---
+++ touhou_project/base/pch.h
@@ -0,0 +1,25 @@
+/**
+ * @file pch.h
+ * @brief 미리 컴파일 처리할 헤더 모음
+ */
+
+#ifndef WINDOW_API_DEFAULT_PCH_H_
+#define WINDOW_API_DEFAULT_PCH_H_
+
+#include "targetver.h"
+
+// Window API from framework.h
+#include <malloc.h>
+#include <memory.h>
+#include <stdlib.h>
+#include <tchar.h>
+#include <windows.h>
+
+// standard library
+#include <assert.h>
+#include <list>
+#include <map>
+#include <string>
+#include <vector>
+
+#endif // WINDOW_API_DEFAULT_PCH_H_
