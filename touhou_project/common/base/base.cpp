a0572d00ebab7902f535c5b1f8dffd524eb070ad
TraceofLight <rickyjun96@gmail.com>
2024-07-15 21:54:15 +0900

    feat: base init
    클래스 관리를 위한 베이스 클래스 추가

---
+++ touhou_project/common/base/base.cpp
@@ -0,0 +1,13 @@
+﻿/**
+ * @file base.cpp
+ * @brief
+ */
+
+#include "base.h"
+
+UINT Base::global_serial_number_ = 0;
+
+Base::Base() : id_(++global_serial_number_) {}
+
+Base::Base(const Base &other)
+    : id_(++global_serial_number_), name_(other.name_) {}
---
+++ touhou_project/common/base/base.h
@@ -0,0 +1,32 @@
+﻿/**
+ * @file base.h
+ * @brief 모든 클래스가 공통으로 가지는 base class
+ */
+
+#ifndef TOUHOU_BASE_H_
+#define TOUHOU_BASE_H_
+
+#include "common/common_utility.h"
+
+class Base {
+private:
+  // 프로그램 전역에서 관리하는 값
+  static UINT global_serial_number_;
+
+  // 각 인스턴스가 발급 받는 값
+  const UINT id_;
+  wstring name_;
+
+public:
+  Base();
+  Base(const Base &other);
+  virtual ~Base() = default;
+  virtual Base *Clone() = 0;
+
+  // Getter & Setter
+  const wstring &name() { return name_; }
+  void SetName(const wstring &name) { name_ = name; }
+  UINT id() const { return id_; }
+};
+
+#endif // TOUHOU_BASE_H_
