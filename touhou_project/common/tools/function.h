06122a63abbe89983201203a22b759671e0b0f42
TraceofLight <rickyjun96@gmail.com>
2024-07-24 02:49:38 +0900

    feat: wrapper
    단일 핸들 오브젝트 소멸자 추가 및 위치 이동

--- touhou_project/common/tools/function.h
+++ touhou_project/common/tools/function.h
@@ -12,17 +12,4 @@
 
 inline bool IsSame(double a, double b) { return fabs(a - b) < LDBL_EPSILON; }
 
-struct HandleObjectDeleterWrapper {
-  template <typename T> static void HandleObjectDeleter(T &object_list) {
-    for (int i = 0; object_list[i] != nullptr; ++i) {
-      DeleteObject(object_list[i]);
-    }
-    delete[] object_list;
-  }
-
-  template <typename T> void operator()(T *object) const {
-    HandleObjectDeleter(object);
-  }
-};
-
 #endif // TOUHOU_FUNCTION_H_
