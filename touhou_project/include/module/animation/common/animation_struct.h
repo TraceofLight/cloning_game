8f231600ab4a1590165fb8563b99d49a2c6f1a84
TraceofLight <rickyjun96@gmail.com>
2024-07-24 21:46:20 +0900

    feat: animation
    animation 구조체 생성 및 관련 메서드 업데이트

---
+++ touhou_project/include/module/animation/common/animation_struct.h
@@ -0,0 +1,29 @@
+﻿/**
+ * @file animation_struct.h
+ * @brief animation 모듈에서 사용할 자료 구조를 모아둔 헤더
+ */
+
+#ifndef TOUHOU_MODULE_COMMON_ANIMATION_STRUCT_H_
+#define TOUHOU_MODULE_COMMON_ANIMATION_STRUCT_H_
+
+#include "common/common_utility.h"
+
+class Texture;
+
+struct AnimationDescription {
+  wstring name_;
+  Texture *atlas_;
+  Vector2 start_left_top_;
+  Vector2 slice_size_;
+  int frame_count_;
+  int fps_;
+};
+
+struct AnimationFrame {
+  Vector2 left_top_;
+  Vector2 slice_;
+  Vector2 offset_;
+  float duration_;
+};
+
+#endif // TOUHOU_MODULE_COMMON_ANIMATION_STRUCT_H_
