58e6c36c108510d72415db3b90c420f67424ab8a
TraceofLight <rickyjun96@gmail.com>
2024-07-29 21:52:31 +0900

    feat: ui init
    button 클래스 구현에 앞서 ui 베이스 클래스 추가

---
+++ touhou_project/include/object/ui/ui.h
@@ -0,0 +1,60 @@
+﻿/**
+ * @file ui.h
+ * @brief 전체 UI에 해당하는 오브젝트들의 base header
+ */
+
+#ifndef TOUHOU_OBJECT_UI_H_
+#define TOUHOU_OBJECT_UI_H_
+
+#include "include/object/object.h"
+
+class UI : public Object {
+  // TODO(KHJ): UI 처리할 수 있는 Manager 추가 필요
+  // friend class UIManager;
+private:
+  UI *parent_ = nullptr;
+  vector<UI *> child_vector_;
+  Vector2 final_position_;
+
+  bool is_mouse_on_ = false;          // 현재 마우스가 UI 위에 있는지
+  bool is_mouse_on_previous_ = false; // 이전 프레임에 마우스가 UI 위에 있었는지
+  bool is_left_button_down_ = false;  // UI가 마우스 왼쪽 버튼이 눌린 상태인지
+
+public:
+  UI();
+  ~UI() override;
+  UI(const UI &other);
+  virtual UI *Clone() override = 0;
+
+  virtual void Tick() override final;
+  virtual void FinalTick() override;
+  virtual void Render() override final;
+
+  virtual void TickUI() = 0;
+  virtual void RenderUI();
+
+  virtual void BeginHover(); // 마우스가 UI 위에 막 올라왔을 때
+  virtual void OnHover();    // 마우스 UI 위에 있을 때
+  virtual void EndHover();   // 마우스가 UI를 빠져 나가는 순간
+
+  virtual void LeftButtonDown();
+  virtual void LeftButtonUp();
+  virtual void LeftButtonClicked();
+
+  // Getter & Setter
+  UI *parent() const { return parent_; }
+  const vector<UI *> &child_vector() const { return child_vector_; }
+  Vector2 final_position() const { return final_position_; }
+  bool is_mouse_hover() const { return is_mouse_on_; }
+  bool is_left_button_down() const { return is_left_button_down_; }
+  void set_parent(UI *parent) { parent_ = parent; }
+  void add_child(UI *child) {
+    child->set_parent(this);
+    child_vector_.push_back(child);
+  }
+
+private:
+  virtual void CheckMouseOn();
+};
+
+#endif // TOUHOU_OBJECT_UI_H_
---
+++ touhou_project/src/object/ui/ui.cpp
@@ -0,0 +1,76 @@
+﻿/**
+ * @file ui.cpp
+ * @brief
+ **/
+
+#include "include/object/ui/ui.h"
+
+#include "common/drawing_handle/drawing_handle.h"
+#include "include/core/engine/engine.h"
+#include "include/manager/key_manager.h"
+
+UI::UI() = default;
+
+UI::~UI() { ReleaseVector(child_vector_); }
+
+UI::UI(const UI &other) : Object(other) {
+  for (const auto iter : other.child_vector())
+    add_child(iter->Clone());
+}
+
+void UI::Tick() {
+  final_position_ = position();
+  if (parent_)
+    final_position_ += parent_->final_position();
+}
+
+void UI::FinalTick() {
+  Object::FinalTick();
+  for (const auto iter : child_vector_)
+    iter->FinalTick();
+}
+
+/**
+ * @brief UI 렌더링하는 함수, 본인을 렌더링하고 child도 순차적으로 렌더링한다.
+ */
+void UI::Render() {
+  RenderUI();
+  for (const auto iter : child_vector_)
+    iter->Render();
+}
+
+void UI::RenderUI() {
+  PEN_TYPE pen_type = PEN_TYPE::GREEN;
+
+  if (is_mouse_on_)
+    pen_type = PEN_TYPE::BLUE;
+  if (is_left_button_down_)
+    pen_type = PEN_TYPE::RED;
+
+  DrawingHandle pen_handle(Engine::Get()->GetBackDC(), pen_type);
+  DrawingHandle brush_handle(Engine::Get()->GetBackDC(), BRUSH_TYPE::HOLLOW);
+  const Vector2 scale = Object::scale();
+
+  Rectangle(Engine::Get()->GetBackDC(), static_cast<int>(final_position_.x()),
+            static_cast<int>(final_position_.y()),
+            static_cast<int>(scale.x()) + static_cast<int>(scale.x()),
+            static_cast<int>(scale.y()) + static_cast<int>(scale.y()));
+}
+void UI::CheckMouseOn() {
+  // 마우스가 UI 위에 올라와있는지 확인한다.
+  is_mouse_on_previous_ = is_mouse_on_;
+
+  const Vector2 mouse_position = KeyManager::Get()->mouse_position();
+  const Vector2 scale = Object::scale();
+
+  const bool is_in_condition =
+      final_position_.x() <= mouse_position.x() &&
+      mouse_position.x() <= final_position_.x() + scale.x() &&
+      final_position_.y() <= mouse_position.y() &&
+      mouse_position.y() <= final_position_.y() + scale.y();
+
+  if (is_in_condition)
+    is_mouse_on_ = true;
+  else
+    is_mouse_on_ = false;
+}
