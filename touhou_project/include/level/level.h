bbd8dea20424421c7d3787317e1c2ed1092a010e
TraceofLight <rickyjun96@gmail.com>
2024-07-31 21:29:34 +0900

    feat: level init
    layer 구분을 위한 level 클래스 생성 및 분류를 위한 object 멤버 변수로 추가

---
+++ touhou_project/include/level/level.h
@@ -0,0 +1,44 @@
+﻿/**
+ * @file level.h
+ * @brief 일종의 Layer 역할을 하는 클래스의 헤더
+ */
+
+#ifndef TOUHOU_LEVEL_H_
+#define TOUHOU_LEVEL_H_
+
+#include "common/base/base.h"
+#include "include/object/object.h"
+#include "include/object/player/player.h"
+
+class Level : public Base {
+private:
+  vector<Object *> layer_list_[LAYER_TYPE::END];
+  Player *current_player_;
+
+public:
+  CLONE_DISABLE(Level)
+  Level() = default;
+  ~Level() override;
+
+  virtual void Init() = 0;
+  virtual void Exit() = 0;
+  virtual void Tick();
+  virtual void FinalTick();
+  virtual void Render();
+
+  void DeleteObjects(LAYER_TYPE type);
+  void DeleteAllObjects();
+
+  // Getter & Setter
+  Player *current_player() const { return current_player_; }
+  vector<Object *> &layer(const LAYER_TYPE type) {
+    return layer_list_[static_cast<int>(type)];
+  }
+  void set_current_player(Player *player) { current_player_ = player; }
+  void add_object_to_layer(Object *object, const LAYER_TYPE type) {
+    object->set_layer_type(type);
+    layer_list_->push_back(std::move(object));
+  }
+};
+
+#endif // TOUHOU_LEVEL_H_
---
+++ touhou_project/src/level/level.cpp
@@ -0,0 +1,42 @@
+﻿/**
+ * @file level.cpp
+ * @brief
+ */
+
+#include "include/level/level.h"
+
+Level::~Level() {
+  for (int i = 0; i < static_cast<int>(LAYER_TYPE::END); ++i)
+    ReleaseVector(layer_list_[i]);
+}
+
+void Level::Tick() {
+  for (vector<Object *> single_layer : layer_list_) {
+    for (Object *layer_object : single_layer)
+      layer_object->Tick();
+  }
+}
+
+void Level::FinalTick() {
+  for (vector<Object *> single_layer : layer_list_) {
+    for (Object *layer_object : single_layer)
+      layer_object->FinalTick();
+  }
+}
+
+void Level::Render() {
+  for (vector<Object *> single_layer : layer_list_) {
+    for (Object *layer_object : single_layer)
+      layer_object->Render();
+  }
+}
+void Level::DeleteObjects(LAYER_TYPE type) {
+  for (const Object *layer_object : layer(type))
+    delete layer_object;
+}
+
+void Level::DeleteAllObjects() {
+  for (int i = 0; i < static_cast<int>(LAYER_TYPE::END); ++i) {
+    DeleteObjects(static_cast<LAYER_TYPE>(i));
+  }
+}
